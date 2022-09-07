dc2b5b27a9c10c8367cb94ea3906d0bdf553c215
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-07 14:57:54 -0300

    Adição de novas habilidades

--- src/Components/Habilidades/Habilidades.tsx
+++ src/Components/Habilidades/Habilidades.tsx
@@ -3,30 +3,64 @@ import styled from 'styled-components'
 import Skills from './Skills'
 import { SiHtml5 as HtmlImg, SiJavascript as JSImg } from 'react-icons/si'
 import { SiTypescript as TSImg, SiStyledcomponents as StyledImg } from 'react-icons/si'
-import { DiCss3 as CSSImg } from 'react-icons/di'
+import { DiCss3 as CSSImg, DiSass as SassImg } from 'react-icons/di'
 import { FaReact as ReactImg } from 'react-icons/fa'
-import { FcSmartphoneTablet as PhoneImg } from 'react-icons/fc'
+import { GiSmartphone as PhoneImg } from 'react-icons/gi'
+import { SiTailwindcss as TailImg } from 'react-icons/si'
+import { RiEnglishInput as ENImg } from 'react-icons/ri'
+import { AiFillGithub as GitImg } from 'react-icons/ai'
 import Head from '../Helper/Head'
 
-const ContainerMain = styled.main`
+const ContainerSkills = styled.section`
   display: flex;
   align-items: center;
   justify-content: space-evenly;
   flex-wrap: wrap;
+  gap: 10px;
+  padding: 10px;
+`
+
+const Title = styled.h1`
+  color: #04c2c9;
+  margin: 20px auto;
+  width: fit-content;
+  text-align: center;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 50%;
+    height: 5px;
+    margin: 0 auto;
+    border-radius: 3px;
+    background-color: #333;
+  }
 `
 
 function Habilidades() {
   return (
     <>
       <Head title='Habilidades' />
-      <ContainerMain className='slide-in-top'>
+      <Title>Minhas habilidades</Title>
+      <ContainerSkills className='slide-in-top'>
         <Skills title='HTML5' image={<HtmlImg />} />
         <Skills title='CSS3' image={<CSSImg />} />
         <Skills title='JavaScript' image={<JSImg />} />
         <Skills title='React' image={<ReactImg />} />
         <Skills title='TypeScript' image={<TSImg />} />
         <Skills title='Styled-Components' image={<StyledImg />} />
-      </ContainerMain>
+      </ContainerSkills>
+      <Title>Outros Conhecimentos</Title>
+      <ContainerSkills className='slide-in-top'>
+        <Skills title='Responsividade' image={<PhoneImg />} />
+        <Skills title='TailwindCSS' image={<TailImg />} />
+        <Skills title='SCSS' image={<SassImg />} />
+        <Skills title='Git/Github' image={<GitImg />} />
+      </ContainerSkills>
+      <Title>Idiomas</Title>
+      <ContainerSkills className='slide-in-top'>
+        <Skills title='Inglês' image={<ENImg />} />
+      </ContainerSkills>
     </>
   )
 }
--- src/Theme/Animation.tsx
+++ src/Theme/Animation.tsx
@@ -96,33 +96,6 @@ export const GlobalAnimations = createGlobalStyle`
     }
   }
 
-  .heartbeat {
-	  animation: heartbeat 1.5s ease-in-out infinite both;
-  }
-  @keyframes heartbeat {
-    from {
-      transform: scale(1);
-      transform-origin: center center;
-      animation-timing-function: ease-out;
-    }
-    10% {
-      transform: scale(0.91);
-      animation-timing-function: ease-in;
-    }
-    17% {
-      transform: scale(0.98);
-      animation-timing-function: ease-out;
-    }
-    33% {
-      transform: scale(0.87);
-      animation-timing-function: ease-in;
-    }
-    45% {
-      transform: scale(1);
-      animation-timing-function: ease-out;
-    }
-  }
-
   .slide-in-top {
 	  animation: slide-in-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
   }
