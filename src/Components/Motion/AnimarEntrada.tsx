672c908755d69bc66741036bf12dc32e70629e1c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-08 00:50:10 -0300

    Criando animação da página

--- src/Components/Motion/AnimarEntrada.tsx
+++ src/Components/Motion/AnimarEntrada.tsx
@@ -2,23 +2,23 @@ import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
 const animations = {
-  initial: { x: 100, opacity: 0 },
-  animate: { x: 0, opacity: 1 },
-  exit: { x: 100, opacity: 0 }
+  initial: { scaleY: 0 },
+  animate: { scaleY: 1 },
+  exit: { scaleY: 0 }
 }
 
 function AnimarEntrada({ children }: any) {
   const id = React.useId()
 
   return (
-    <AnimatePresence>
+    <AnimatePresence mode='wait'>
       <motion.div
       key={id}
       variants={animations}
       initial='initial'
       animate='animate'
       exit='exit'
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 0.3 }}
       >
         {children}
       </motion.div>
