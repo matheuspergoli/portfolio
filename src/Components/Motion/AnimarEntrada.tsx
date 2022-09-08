b3e2071cab84977f1f98a64a2af9438920f769a4
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-07 21:18:57 -0300

    Adicionando Motion

---
+++ src/Components/Motion/AnimarEntrada.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const animations = {
+  initial: { scaleY: 0 },
+  animate: { scaleY: 1 },
+  exit: { scaleY: 0 }
+}
+
+function AnimarEntrada({ children }: any) {
+  const id = React.useId()
+
+  return (
+    <AnimatePresence>
+      <motion.div
+      key={id}
+      variants={animations}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      transition={{ duration: 0.5 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
+
+export default AnimarEntrada
