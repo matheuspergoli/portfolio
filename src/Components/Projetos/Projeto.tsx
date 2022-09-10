66bfd1a94f357faeab0971829b487143006c8c9e
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-10 02:00:37 -0300

    Adicionando mais infos de projetos e colocando links para o site e rep

--- src/Components/Projetos/Projeto.tsx
+++ src/Components/Projetos/Projeto.tsx
@@ -17,51 +17,87 @@ import Trillo from '../../images/trillo.jpg'
 const projects = [
   {
     nome: 'Clipboard Landing Page',
-    source: Clipboard
+    source: Clipboard,
+    tech: 'HTML/CSS & JS',
+    link: 'https://matheuspergoli.github.io/clipboard-flexbox/',
+    rep: 'https://github.com/matheuspergoli/clipboard-flexbox'
   },
   {
     nome: 'Desafio Rafa Ballerini',
-    source: Desafio
+    source: Desafio,
+    tech: 'HTML/CSS & JS',
+    link: 'https://matheuspergoli.github.io/balle-desafio-extra/',
+    rep: 'https://github.com/matheuspergoli/balle-desafio-extra'
   },
   {
     nome: 'Four Card Section',
-    source: FourCard
+    source: FourCard,
+    tech: 'HTML/CSS',
+    link: 'https://matheuspergoli.github.io/four-card-feature/',
+    rep: 'https://github.com/matheuspergoli/four-card-feature'
   },
   {
     nome: 'Huddle Landing Page',
-    source: Huddle
+    source: Huddle,
+    tech: 'HTML/CSS & JS',
+    link: 'https://matheuspergoli.github.io/huddle-landing-page_v2/',
+    rep: 'https://github.com/matheuspergoli/huddle-landing-page_v2'
   },
   {
     nome: 'Netflix Login',
-    source: Netflix
+    source: Netflix,
+    tech: 'HTML/SCSS',
+    link: 'https://matheuspergoli.github.io/netflix-login/',
+    rep: 'https://github.com/matheuspergoli/netflix-login'
   },
   {
     nome: 'Nexter',
-    source: Nexter
+    source: Nexter,
+    tech: 'HTML/SCSS',
+    link: 'https://matheuspergoli.github.io/nexter-project/',
+    rep: 'https://github.com/matheuspergoli/nexter-project'
   },
   {
     nome: 'NFT Card React',
-    source: NftReact
+    source: NftReact,
+    tech: 'React/Typescript & Styled-Components',
+    link: 'https://nft-card-react-itobuexs0-matheuspergoli.vercel.app/',
+    rep: 'https://github.com/matheuspergoli/nft-card-react'
   },
   {
     nome: 'Portfolio freeCodeCamp',
-    source: Portfolio
+    source: Portfolio,
+    tech: 'HTML/CSS & JS',
+    link: 'https://matheuspergoli.github.io/fcc_project-n5_web/',
+    rep: 'https://github.com/matheuspergoli/fcc_project-n5_web'
   },
   {
     nome: 'Snap Intro Section',
-    source: Snap
+    source: Snap,
+    tech: 'HTML/SCSS & JS',
+    link: 'https://matheuspergoli.github.io/snap-intro-section/',
+    rep: 'https://github.com/matheuspergoli/snap-intro-section'
   },
   {
     nome: 'Summary Component React',
-    source: SummaryReact
+    source: SummaryReact,
+    tech: 'React/Typescript & Styled-Components',
+    link: 'https://summary-component-react-1bjn1cejd-matheuspergoli.vercel.app/',
+    rep: 'https://github.com/matheuspergoli/summary-component-react'
   },
   {
     nome: 'Testimonials',
-    source: Testimonials
+    source: Testimonials,
+    tech: 'HTML/CSS',
+    link: 'https://matheuspergoli.github.io/testimonials-grid/',
+    rep: 'https://github.com/matheuspergoli/testimonials-grid'
   },
   {
     nome: 'Trillo',
-    source: Trillo
+    source: Trillo,
+    tech: 'HTML/SCSS',
+    link: 'https://matheuspergoli.github.io/trillo-project/',
+    rep: 'https://github.com/matheuspergoli/trillo-project'
   }
 ]
 
@@ -101,6 +137,49 @@ const Title = styled(motion.h1)`
   }
 `
 
+const ImageContainer = styled.figure`
+  position: relative;
+  
+  figcaption {
+    position: absolute;
+    inset: 0;
+    display: flex;
+    align-items: center;
+    opacity: 0;
+    transition: all 300ms;
+    justify-content: space-evenly;
+    flex-direction: column;
+    background-color: #333;
+    color: white;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    span {
+      color: #04c2c9;
+    }
+
+    p {
+      color: white;
+    }
+  }
+`
+
+const LinksContainer = styled.section`
+  display: flex;
+  gap: 10px;
+
+  a {
+    display: block;
+    padding: 10px;
+    color: #333;
+    font-size: 1.5rem;
+    text-decoration: none;
+    background-color: #04c2c9;
+  }
+`
+
 const animations = {
   initial: { opacity: 0 }
 }
@@ -116,7 +195,16 @@ function Projeto() {
         whileInView={{ opacity: 1 }}
         key={projeto.nome}>
           <Title>{projeto.nome}</Title>
-          <img src={projeto.source} alt={projeto.nome} />
+          <ImageContainer>
+            <img src={projeto.source} alt={projeto.nome} />
+            <figcaption>
+              <p>Tech: <span>{projeto.tech}</span></p>
+              <LinksContainer>
+                <a href={projeto.link} target='_blank'>Ir para site</a>
+                <a href={projeto.rep} target='_blank'>Repositório</a>
+              </LinksContainer>
+            </figcaption>
+          </ImageContainer>
         </ContainerProjeto>
       ))}
     </Container>
