036257e4f7f5a3f424cb1a8e2822e2df499e94ac
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-12 20:09:03 -0300

    Melhorando o texto sobre mim

--- src/Components/Sobre/SobreMim.tsx
+++ src/Components/Sobre/SobreMim.tsx
@@ -40,19 +40,16 @@ function SobreMim() {
         <Title>Quem sou eu?</Title>
         <Sobre>
           <p>
-            Me chamo <span>Matheus Pergoli</span> Tenho 23 anos, sou de SP/Brasil
-            e sou estudante de <span>Desenvolvimento Web</span>.
+            Sou <span>Matheus Pergoli</span>, tenho 23 anos, sou de SP - Capital e estudo <span>Desenvolvimento Web</span>.
           </p>
           <p>
-            No começo de tudo, <span>Python</span> foi meu primeiro contato com linguagem de programação na plataforma
-            do Curso em Vídeo, lá foi onde tudo começou, mas logo pulei para o <span>HTML/CSS</span> e então descobri o que eu gostava de fazer. 
+            No começo de tudo <span>Python</span> foi meu primeiro contato com a linguagem de programação na plataforma do Curso em Vídeo. Foi lá onde tudo começou. Logo pulei para o <span>HTML/CSS</span> e então descobri o que realmente gosto de fazer. 
           </p>
           <p>
-            Sempre achei muito divertido ver o resultado imediato de tudo o que eu fazia, e com o tempo isso só foi me trazendo mais desejo pelo conhecimento e descobertas sobre o gigante universo do <span>Desenvolvimento Front-end</span>.
+            Acho gratificante ver o resultado imediato de tudo o que faço. Com o tempo isso foi me trazendo mais desejo pelo conhecimento e descobertas sobre o gigantesco universo do <span>Desenvolvimento Front-end</span>.
           </p>
           <p>
-            Hoje é um prazer para mim estudar sobre esse universo,
-            a cada dia que passa minha vontade por estudar e conhecer mais desse mundo só aumenta, atráves de dificuldades, erros e acertos e muita dor de cabeça, meu desejo de construir uma <span>Carreira</span> nessa área permanece intacto.
+          Hoje, um de meus maiores prazeres é estudar sobre esse universo. A cada dia que passa minha vontade por estudar e conhecer mais desse mundo só aumenta. Por meio de dificuldades, erros, acertos, muita dedicação e carinho, meu desejo de construir uma <span>Carreira</span> nessa área permanece imensurável.
           </p>
         </Sobre>
       </ContainerMain>
