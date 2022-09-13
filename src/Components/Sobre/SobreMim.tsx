a71725900df66cc192cb81d46e65de2cf64fc7b9
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-12 21:51:41 -0300

    Adicionando uma parte no sobre mim para saber mais

--- src/Components/Sobre/SobreMim.tsx
+++ src/Components/Sobre/SobreMim.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import styled from 'styled-components'
 import Head from '../Helper/Head'
@@ -32,12 +33,49 @@ const Title = styled.h1`
   }
 `
 
+const SaberMaisTitle = styled.h1`
+  font-size: 1.6rem;
+  text-align: center;
+  margin: 20px 0;
+`
+
+const SaberMaisContainer = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  padding: 20px;
+  margin: 0 auto;
+  cursor: pointer;
+  overflow: hidden;
+  text-align: center;
+  background-color: #333;
+
+  & ::selection {
+    background-color: transparent;
+  }
+
+  &[data-isOpen="true"] {
+    height: auto;
+    width: 100%;
+    max-width: 650px;
+
+    & > p {
+      color: white;
+
+      span {
+        color: #04c2c9;
+      }
+    }
+  }
+`
+
 function SobreMim() {
+  const [isOpen, setIsOpen] = React.useState(false)
+
   return (
     <AnimateFadeDiv>
       <Head title='Sobre mim' />
       <ContainerMain>
-        <Title>Quem sou eu?</Title>
+        <Title>Quem sou eu ?</Title>
         <Sobre>
           <p>
             Sou <span>Matheus Pergoli</span>, tenho 23 anos, sou de SP - Capital e estudo <span>Desenvolvimento Web</span>.
@@ -52,6 +90,20 @@ function SobreMim() {
           Hoje, um de meus maiores prazeres é estudar sobre esse universo. A cada dia que passa minha vontade por estudar e conhecer mais desse mundo só aumenta. Por meio de dificuldades, erros, acertos, muita dedicação e carinho, meu desejo de construir uma <span>Carreira</span> nessa área permanece imensurável.
           </p>
         </Sobre>
+
+        <SaberMaisTitle>Gostaria de saber um pouco mais ?</SaberMaisTitle>
+        <SaberMaisContainer
+        layout
+        initial={{ borderRadius: 50 }}
+        data-isopen={isOpen}
+        onClick={() => setIsOpen(!isOpen)}>
+          <p>Eu também gosto muito de jogar, meu estilo de jogo preferido é MMORPG. (Sou péssimo em FPS 😅)</p>
+          <p>Já fui viciado em World of Warcraft, mas consegui me livrar, porém quem sabe na próxima expansão. 🙄</p>
+          <p>Hoje meu gosto é bem variado e venho jogando diversos jogos quando não estou estudando.</p>
+          <p>Também adoro músicas, sempre que estou praticando estou ouvindo algo.</p>
+          <p>Séries e filmes nem se fala, quase todo dia vejo um filme novo.</p>
+          <p>Caso queira ter uma conversa mais descontraída me add no Discord <span>Matheus🌙#6691</span></p>
+        </SaberMaisContainer>
       </ContainerMain>
     </AnimateFadeDiv>
   )
