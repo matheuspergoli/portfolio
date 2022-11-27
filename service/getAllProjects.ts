48504cdc8f78f31173fedf48bc53910131c31eed
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-27 00:30:06 -0300

    Update

--- service/getAllProjects.ts
+++ service/getAllProjects.ts
@@ -4,7 +4,7 @@ const endpoint = 'https://graphql.datocms.com/'
 
 const query = gql`
 	query {
-		allProjetos(orderBy: _createdAt_ASC) {
+		allProjetos(orderBy: _createdAt_ASC, first: "30") {
 			id
 			nome
 			link
